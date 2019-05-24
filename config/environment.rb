c8ab5e233a9d49bb7fa606f612c2dfdac7825578
github username <github email address>
2019-05-24 16:59:01 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,20 @@ require 'active_record'
 require 'yaml/store'
 require 'ostruct'
 require 'date'
+require 'pry'
 
 
 require 'bundler/setup'
 Bundler.require
 
 
-# put the code to connect to the database here
+require 'bundler/setup'
+Bundler.require
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 
 
 require_relative "../artist.rb"
